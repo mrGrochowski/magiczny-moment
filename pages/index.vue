1df451b12c542b87cdbda591236c17459a28d1fc
mrGrochowski <mateusz.grochowski1992@gmail.com>
2024-11-11 05:51:04 +0100

    add hero section and tailwind

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,15 @@
 <template>
 
-
   <section v-for="section in sections" :key="section._id">
     <pre>{{ section }}</pre>
-    <img :src="section.image" alt="section.title" />
+    <template v-if="section._id === 'content:hero.md'">
+      <Hero :image="section.image" />
+    </template>
     <!-- <ContentRendererMarkdown :value="section.body" /> -->
   </section>
 </template>
 
-<script setup>
+<script setup lang="ts">
 const { data: sections } = await useAsyncData('sections', async () => {
   return await queryContent().find()
 })
