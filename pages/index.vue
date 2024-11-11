14086884e2cc7421486037d6ff3b2f8f2e0fffe9
mrGrochowski <mateusz.grochowski1992@gmail.com>
2024-11-11 04:34:35 +0100

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    BĘDĘ MIAŁ SEKCJE
+
+    <main>
+      <pre>{{ data }}</pre>
+    </main>
+  </div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute();
+
+const { data } = await useAsyncData(`content-${route.path}`, () => {
+  return queryContent().where({ _partial: true }).findOne();
+})
+
+</script>
+
+<style scoped></style>
